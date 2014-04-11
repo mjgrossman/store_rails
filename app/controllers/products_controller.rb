78a17fdae9b2adb92b658f1f92d7d148af54fae6
Harland Hauger & Michael Grossman <student@epicodus.com>
2014-04-11 09:55:06 -0700

    products RESTful

---
+++ app/controllers/products_controller.rb
@@ -0,0 +1,44 @@
+class ProductsController < ApplicationController
+
+  def index
+    @products = Product.all
+  end
+
+  def show
+    @product = Product.find(params[:id])
+  end
+
+  def new
+    @product = Product.new
+  end
+
+  def edit
+    @product = Product.find(params[:id])
+  end
+
+  def update
+    @product = Product.find(params[:id])
+    if params[:product]
+      @product.update(products_params)
+    end
+      redirect_to product_path
+  end
+
+  def create
+    @product = Product.create(products_params)
+    redirect_to(root_path)
+  end
+
+  def destroy
+    @product = Product.find(params[:id])
+    @product.destroy
+    redirect_to(root_path)
+  end
+
+private
+
+  def products_params
+    params.require(:product).permit(:id, :name, :price, :description)
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 StoreV2::Application.routes.draw do
+  root to: 'home#index'
   resources :products
 end
